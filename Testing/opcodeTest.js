f066c0a1eaf8aef9dacc0f94dc5b728ec3935465
Mathieu Laflamme <mlaflamm@sfu.ca>
2019-02-03 20:52:43 -0800

    [Nishan] added 1nnn and 2nnn

--- Testing/opcodeTest.js
+++ Testing/opcodeTest.js
@@ -49,7 +49,44 @@ function clrDisp() {
 }
 
 //add functions ret, one NNN and two NNN here
+function oneNNN {
 
+    chip.oneCycle(0x00E0);
+    if (chip.programCounter == 00E0) {
+        works = true;
+    }
+
+    if (!works) {
+
+        console.log(“Opcode 1 NNN: Failed”)
+    } else {
+        console.log(“Opcode 1 NNN: Pass”);
+    }
+}
+
+function twoNNN {
+    works = false
+    let tempSp = chip.stackPointer;
+    let tempPC = chip.programCounter;
+    chip.oneCycle(0X2ABC);
+    if (tempSp !== chip.stackPointer) {
+        works = false;
+    }
+    if (chip.stack[chip.stackPointer] !== tempPC) {
+        works = false;
+    }
+
+    if (chip.programCounter !== 0xABC)[
+        works = true;
+}
+if (works === true) {
+
+    console.log(“Opcode 2 NNN: Failed”)
+} else {
+    console.log(“Opcode 2 NNN: Pass”);
+}
+}
+}
 
 function threeXKK() { //opcode 0x3xkk, if register Vx == kk, skip to next instruction (PC + 2)
                         //this one has two cases, one where Vx == kk and one where Vx != kk. We need to test both
